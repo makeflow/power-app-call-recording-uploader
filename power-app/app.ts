fddecbe72ff4f620d900418b2b14aa755ce2c7ca
SUCHMOKUO <suchmokuo@gmail.com>
2020-07-26 23:38:29 +0800

    update config

--- power-app/app.ts
+++ power-app/app.ts
@@ -1,9 +1,8 @@
 import {PowerApp} from '@makeflow/power-app';
 import Koa from 'koa';
-import {port} from './config';
+import {port, version} from './config';
 import controllers from './controller';
 import {response} from './middleware';
-import {version} from './power-app.json';
 import powerNodes from './power-nodes';
 
 const powerApp = new PowerApp();
--- power-app/config.ts
+++ power-app/config.ts
@@ -1,17 +1,15 @@
+export let version: string;
 export let port: number;
 export let uploadEndpoint: string;
 
 if (process.env.NODE_ENV === 'production') {
-  const portStr = process.env.PORT;
-
-  if (!portStr) {
-    throw new Error("env 'PORT' not set!");
-  }
-
-  port = Number(portStr);
+  version = process.env.VERSION as string;
+  port = Number(process.env.PORT);
   uploadEndpoint =
     'https://power-apps.makeflow.com/call-recording-uploader/upload-record';
 } else {
-  port = 9999;
+  const config = require('./power-app.json');
+  version = config.version;
+  port = Number(config.hookBaseURL.split(':')[2]);
   uploadEndpoint = '';
 }
