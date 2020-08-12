a39ca498493c8cb2016560520ff2ef1189908996
SUCHMOKUO <suchmokuo@gmail.com>
2020-08-13 01:18:21 +0800

    Add auto-version script for android

--- release.sh
+++ release.sh
@@ -21,16 +21,25 @@ fi
 case $TYPE in
 android)
   TAG="android-release-v$VER"
+  cd android-app
   ;;
+
 power-app)
   TAG="power-app-v$VER"
+  cd power-app
   ;;
+
 *)
   echo "unknown type: '$TYPE'"
   exit 1
   ;;
 esac
 
+npm --no-git-tag-version --allow-same-version version "$VER"
+cd -
+
+git add .
+git commit -m "$TAG"
 git tag $TAG
 git push
 git push origin $TAG
